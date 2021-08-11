0de887b1ba3b6de4de5df3490cffca49205ac264
Victoshik <88432030+Victoshik@users.noreply.github.com>
2021-08-11 15:55:21 +0300

    Delete task_1_3.py

--- src/task_1_3.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Enter the side of cube"))
-v = a ** 3
-s = (a ** 2) * 6
-print("Cube volume =", v, "Lateral surface area", s)
-# Дана длина ребра куба.
-# Найти объем куба и площадь его боковой поверхности.
